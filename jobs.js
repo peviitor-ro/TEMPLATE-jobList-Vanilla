34561635240ffe0807deb51af34755ff36bcdb92
Claudiu Sonica <csonica95@gmail.com>
2023-08-18 20:14:47 +0200

    refactored the function that generates the jobs

--- jobs.js
+++ jobs.js
@@ -1,47 +1,61 @@
-const jobPostings = [
-  {
-    "@context": "https://schema.org",
-    "@type": "JobPosting",
-    title: "Software Engineer",
-    description: "Job description goes here...",
-    datePosted: "2022-01-01",
-    validThrough: "2022-12-31",
-    employmentType: "Full-time",
-    hiringOrganization: {
-      "@type": "Organization",
-      name: "Your Company",
-      sameAs: "https://www.yourcompany.com",
-    },
-    jobLocation: {
-      "@type": "Place",
-      address: {
-        "@type": "PostalAddress",
-        addressLocality: "Oradea",
-      },
+const jobPostings = [];
+
+const templateJobPosting = {
+  "@context": "https://schema.org",
+  "@type": "JobPosting",
+  employmentType: "Full-time",
+  hiringOrganization: {
+    "@type": "Organization",
+    name: "Your Company",
+    sameAs: "https://www.yourcompany.com",
+  },
+  jobLocation: {
+    "@type": "Place",
+    address: {
+      "@type": "PostalAddress",
     },
   },
-  {
-    "@context": "https://schema.org",
-    "@type": "JobPosting",
-    title: "Frontend Developer",
-    description: "Job description goes here...",
-    datePosted: "2022-01-01",
-    validThrough: "2022-12-31",
-    employmentType: "Full-time",
-    hiringOrganization: {
-      "@type": "Organization",
-      name: "Your Company",
-      sameAs: "https://www.yourcompany.com",
-    },
+};
+
+const createJobPosting = (title, description, employmentType, datePosted, validThrough, addressLocality) => {
+  const newJobPosting = {
+    ...templateJobPosting,
+    title,
+    description,
+    employmentType,
+    datePosted,
+    validThrough,
     jobLocation: {
-      "@type": "Place",
+      ...templateJobPosting.jobLocation,
       address: {
-        "@type": "PostalAddress",
-        addressLocality: "Bucharest",
+        ...templateJobPosting.jobLocation.address,
+        addressLocality,
       },
     },
-  },
-  // Add more job postings as needed
-];
+  };
+  
+  jobPostings.push(newJobPosting);
+}
+
+// Example usage
+createJobPosting(
+  "Software Engineer",            // Title
+  "Job description goes here...", // Job description
+  "Full-time",                    // Employment TYpe
+  "2022-01-01",                   // optional Date when is posted
+  "2022-12-31",                   // optional Date when expires
+  "Oradea"                        // City or remote
+);
+
+createJobPosting(
+  "Frontend Developer",
+  "Job description goes here...",
+  "Part-time",
+  "2022-01-01",
+  "2022-12-31",
+  "Cluj-Napoca"
+)
+
+// You can add more job postings using the createJobPosting function
 
 export default jobPostings;
